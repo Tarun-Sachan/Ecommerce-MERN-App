0d6705b351b3fdc0cd9201fc925baa7f7b6b11f3
tarunSachan <117214735+Tarun-Sachan@users.noreply.github.com>
2023-09-09 16:32:13 +0530

    Update ProductImages.js

--- ecommerce-front/components/ProductImages.js
+++ ecommerce-front/components/ProductImages.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 const BigImage = styled.img`
-  max-width: 100%;
-  max-height: 100%;
+  max-width: 25%;
+  max-height: 25%;
 `;
 const Image = styled.img`
   max-width: 100%;
